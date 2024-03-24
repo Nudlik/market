a94eae83fa0da13204e1b266de3e991c467ab2cf
Nudlik <gegorp@mail.ru>
2024-03-24 21:54:51 +0700

    донастроить гуникорн к джанге

--- backend_django/config/urls.py
+++ backend_django/config/urls.py
@@ -23,7 +23,7 @@ from config.yasg import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
