a828c21524092bc50f43b48cd070d368d1c6cbaf
Nudlik <gegorp@mail.ru>
2024-03-25 12:35:23 +0700

    раскидал права на приложение ads

--- backend_django/ads/views.py
+++ backend_django/ads/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
 
 from ads.models import Ad, Comment
 from ads.pagination import AdPagination
+from ads.permissions import EmailOwner
 from ads.serializers import AdSerializer, CommentSerializer, AdDetailSerializer
 
 
@@ -10,13 +12,32 @@ class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
     pagination_class = AdPagination
+    perms_methods = {
+        'list': [AllowAny],
+        'update': [EmailOwner | IsAdminUser],
+        'partial_update': [EmailOwner | IsAdminUser],
+        'destroy': [EmailOwner | IsAdminUser],
+    }
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = AdDetailSerializer(instance)
         return Response(serializer.data)
 
+    def get_permissions(self):
+        self.permission_classes = self.perms_methods.get(self.action, self.permission_classes)
+        return [permission() for permission in self.permission_classes]
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    perms_methods = {
+        'update': [EmailOwner | IsAdminUser],
+        'partial_update': [EmailOwner | IsAdminUser],
+        'destroy': [EmailOwner | IsAdminUser],
+    }
+
+    def get_permissions(self):
+        self.permission_classes = self.perms_methods.get(self.action, self.permission_classes)
+        return [permission() for permission in self.permission_classes]
